23b284baa8e13b2fb8198bee8f29729b348d60de
kevin-maciel <kevin.maciel@moxit.net>
2021-05-05 02:14:27 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import configureStore from './store/configureStore';
+import * as actions from './store/bugs';
+import { projectAdded } from './store/projects'
+
+
+const store = configureStore();
+
+store.subscribe(() => {
+    console.log("Store changed!");
+});
+
+store.dispatch(projectAdded({ name: "Project 1" }));
+store.dispatch(actions.bugAdded({ description : "Bug 1"}));
+store.dispatch(actions.bugAdded({ description : "Bug 2"}));
+store.dispatch(actions.bugAdded({ description : "Bug 3"}));
+store.dispatch(actions.bugResolved({id : 1}));
+
+console.log(store.getState());
